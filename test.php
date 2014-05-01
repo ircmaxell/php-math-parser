5fb4205a7bf08c954936b0d28884e7f5afb996b3
Sean Fahey <sean@seanfahey.org>
2014-04-30 19:06:33 -0500

    Added some test cases for math on floats.

--- test.php
+++ test.php
@@ -27,3 +27,24 @@ $math->registerVariable('a', 5);
 $answer = $math->evaluate('($a + $a) * 4');
 var_dump($answer);
 // int(40)
+
+$answer = $math->evaluate('1.45 + 3');
+var_dump($answer);
+// float 4.45
+
+$answer = $math->evaluate('0.45 + 3.5');
+var_dump($answer);
+// float 3.95
+
+$answer = $math->evaluate('10.6 / 1.2');
+var_dump($answer);
+// float 8.8333333333333
+
+$answer = $math->evaluate('(1.65 + 2) * (3.1415 + 4) * (5 + 6.8989)');
+var_dump($answer);
+// float 310.1623793775
+
+$math->registerVariable('a', 5.36464);
+$answer = $math->evaluate('($a + $a) * 4');
+var_dump($answer);
+// float 42.91712
